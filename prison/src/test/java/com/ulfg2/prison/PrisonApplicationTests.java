e1d93619d37d708909c91994b79b1e481cada0e5
Christolio-Al-Fhaily <christoliofhaily@gmail.com>
2025-01-04 17:48:50 +0200

    init

---
+++ prison/src/test/java/com/ulfg2/prison/PrisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulfg2.prison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
